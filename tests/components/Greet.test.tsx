afedc2836c5c268f43df062042dd8cb99197dc88
Yashraj_K <yashrajkamble7727@gmail.com>
2026-02-03 11:44:15 +0530

    Testing Rendering

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { it, expect, describe } from "vitest";
+import Greet from "../../src/components/Greet";
+import "@testing-library/jest-dom/vitest";
+
+describe("greet", () => {
+  it("should render Hello with the name when name is provided", () => {
+    render(<Greet name={"yash"} />)
+
+    const heading = screen.getByRole('heading')
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/hello yash/i)
+  });
+
+  it("should render login button when name is not provided", () => {
+    render(<Greet />)
+
+    const button = screen.getByRole('button')
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/Login/)
+  });
+});
