ddd6148ebd7a0acedcdee2c159b9998f74581b6c
Yashraj_K <yashrajkamble7727@gmail.com>
2026-02-03 19:02:04 +0530

    Testing Expandable Text

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,8 @@
-import TermsAndConditions from "../components/TermsAndConditions";
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+
+  return <ExpandableText text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum perspiciatis amet veritatis, repellat magnam minima hic et voluptatem voluptate fuga, magni delectus laboriosam dignissimos. Illum aperiam qui molestiae eaque, consectetur esse voluptates dolore officiis est odit fugit consequatur magni beatae, ipsum debitis nulla vero et rerum eius neque. Aliquam, laboriosam odio ex eos sequi consectetur voluptatem iure sunt sed nostrum optio recusandae, commodi aut reprehenderit, corporis adipisci obcaecati et veritatis repellendus. Culpa expedita accusamus ratione quia autem esse excepturi, officiis dolor voluptatem est, magnam optio alias nostrum in non porro quam earum cupiditate dolorum. Ad laborum id eum eligendi debitis." />;
 };
 
 export default PlaygroundPage;
