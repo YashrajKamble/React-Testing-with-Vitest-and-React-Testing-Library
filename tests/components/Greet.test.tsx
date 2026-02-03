49d88fb74babce77a91ef18cc137c82406c3d905
Yashraj_K <yashrajkamble7727@gmail.com>
2026-02-03 15:25:57 +0530

    Simplifying Test Setup

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import { it, expect, describe } from "vitest";
+// import { it, expect, describe } from "vitest";
 import Greet from "../../src/components/Greet";
-import "@testing-library/jest-dom/vitest";
 
 describe("greet", () => {
   it("should render Hello with the name when name is provided", () => {
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
-import {defineConfig} from "vitest/config";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    test:{
-        environment:"jsdom"
-    }
-})
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "test/setup.ts",
+  },
+});
